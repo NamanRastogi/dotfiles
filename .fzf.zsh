a315f3ff03e88febf78452d4d7e98e420f7f6d15
Naman Rastogi <naman.rastogi.52@gmail.com>
2021-09-25 02:30:13 +0530

    Update fzf for theme and removed height

--- .fzf.zsh
+++ .fzf.zsh
@@ -16,6 +16,6 @@ source "$HOME/.fzf/shell/key-bindings.zsh"
 export FZF_DEFAULT_COMMAND='fd --type f --hidden'
 export FZF_DEFAULT_OPTS='--no-height'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
-export FZF_CTRL_T_OPTS="--preview 'bat --color=always --line-range :50 {}'"
+export FZF_CTRL_T_OPTS="--preview 'bat --color=always --theme=TwoDark {}'"
 export FZF_ALT_C_COMMAND='fd --type d . --hidden'
 export FZF_ALT_C_OPTS="--preview 'tree -C {} | head -50'"
