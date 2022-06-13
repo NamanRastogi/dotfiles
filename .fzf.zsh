c3f6ebc1ff935455b3d3f0841f24a808b299d2bc
Naman Rastogi <naman.rastogi.52@gmail.com>
2022-06-14 04:23:30 +0530

    Updated directory preview in fzf from using tree to using exa

--- .fzf.zsh
+++ .fzf.zsh
@@ -18,4 +18,4 @@ export FZF_DEFAULT_OPTS='--no-height'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 export FZF_CTRL_T_OPTS="--preview 'batcat --color=always --theme=TwoDark {}'"
 export FZF_ALT_C_COMMAND='fd --type d . --hidden'
-export FZF_ALT_C_OPTS="--preview 'tree -C {} | head -50'"
+export FZF_ALT_C_OPTS="--preview 'exa --tree --level=2 {}'"
