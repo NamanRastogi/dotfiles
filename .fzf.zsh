79cef30d1a4edaa3ac3c75c0bc05fdf3111185c4
Naman Rastogi <naman.rastogi.52@gmail.com>
2023-12-16 01:45:59 +0530

    Update .fzf.zsh for installing using package manager

--- .fzf.zsh
+++ .fzf.zsh
@@ -6,11 +6,13 @@ fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "$HOME/.fzf/shell/completion.zsh" 2> /dev/null
+# [[ $- == *i* ]] && source "$HOME/.fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source /usr/share/doc/fzf/examples/completion.zsh 2> /dev/null
 
 # Key bindings
 # ------------
-source "$HOME/.fzf/shell/key-bindings.zsh"
+# source "$HOME/.fzf/shell/key-bindings.zsh"
+source /usr/share/doc/fzf/examples/key-bindings.zsh
 
 # Customization
 export FZF_DEFAULT_COMMAND='fd --type f --hidden'
