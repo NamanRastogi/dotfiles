ae77a8097599205ea8f7870c1977700fb3f6588d
Naman Rastogi <naman.rastogi.52@gmail.com>
2024-03-16 15:50:49 +0530

    Update aliases for eza in fzf.zsh

--- .config/fzf.zsh
+++ .config/fzf.zsh
@@ -20,4 +20,4 @@ export FZF_DEFAULT_OPTS='--no-height'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 export FZF_CTRL_T_OPTS="--preview 'bat {}'"
 export FZF_ALT_C_COMMAND='fd --type d . --hidden'
-export FZF_ALT_C_OPTS="--preview 'exa --tree --level=2 {}'"
+export FZF_ALT_C_OPTS="--preview 'eza --tree --level=2 {}'"
