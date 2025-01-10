263da2b7dadb53b22858c12a95f4abc960b53b3b
Naman Rastogi <naman.rastogi.52@gmail.com>
2025-01-10 17:16:14 +0530

    Update history.zsh so that new lines added as soon as they are entered

--- .config/zsh/history.zsh
+++ .config/zsh/history.zsh
@@ -48,4 +48,5 @@ setopt hist_expire_dups_first # delete duplicates first when HISTFILE size excee
 setopt hist_ignore_dups       # ignore duplicated commands history list
 setopt hist_ignore_space      # ignore commands that start with space
 setopt hist_verify            # show command with history expansion to user before running it
-setopt share_history          # share command history data
+# setopt share_history        # share command history data
+set inc_append_history        # new lines added to the $HISTFILE as soon as they are entered
