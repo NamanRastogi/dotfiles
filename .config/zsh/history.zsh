8ea63ad1bf762631c4b9b79b31ce34b1513fe1ec
Naman Rastogi <naman.rastogi.52@gmail.com>
2025-01-29 13:05:13 +0530

    Fix history.zsh

--- .config/zsh/history.zsh
+++ .config/zsh/history.zsh
@@ -49,4 +49,4 @@ setopt hist_ignore_dups       # ignore duplicated commands history list
 setopt hist_ignore_space      # ignore commands that start with space
 setopt hist_verify            # show command with history expansion to user before running it
 # setopt share_history        # share command history data
-set inc_append_history        # new lines added to the $HISTFILE as soon as they are entered
+setopt inc_append_history     # new lines added to the $HISTFILE as soon as they are entered
